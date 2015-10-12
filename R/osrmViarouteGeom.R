752e5f02fd459346a9012af3b77988b9e2f280fd
rCarto <timothee.giraud@ums-riate.fr>
2015-10-12 16:54:51 +0200

    Add the possibility to output SpatialLinesDataFrame with osrmViarouteGeom

--- R/osrmViarouteGeom.R
+++ R/osrmViarouteGeom.R
@@ -6,8 +6,12 @@
 #' @param yo latitude of the origine point.
 #' @param xd longitude of the destination point.
 #' @param yd latitude of the destination point.
+#' @param sp if sp is TRUE the function returns a SpatialLinesDataFrame.
+#' @param ido identifier of the origin point (use if sp is TRUE).
+#' @param idd identifier of the destination point (use if sp is TRUE).
 #' @return A data frame is return. It contains the longitudes and latitudes of 
-#' the travel path between the two points.
+#' the travel path between the two points. If sp is TRUE a SpatialLinesDataFrame 
+#' is return. It contains two fields : identifiers of origine and destination.
 #' @seealso \link{osrmViaroute}
 #' @examples 
 #' # Load data
@@ -19,8 +23,25 @@
 #' plot(com[c(1,15),3:4], asp =1, col = "red", pch = 20, cex = 1.5)
 #' points(routeGeom[,2:1], type = "l", lty = 2)
 #' text(com[c(1,15),3:4], labels = com[c(1,15),2], pos = 2)
+#' 
+#' # Travel path between points - output a SpatialLinesDataFrame
+#' routeGeom2 <- osrmViarouteGeom(xo = com[1,"lon"], yo = com[1,"lat"],
+#'                                xd = com[16,"lon"], yd = com[16,"lat"], 
+#'                                sp=TRUE, 
+#'                                ido = com[1,"comm_id"], 
+#'                                idd = com[16,"comm_id"])
+#' class(routeGeom2)
+#' # Display the path
+#' plot(com[c(1,16),3:4], asp =1, col = "red", pch = 20, cex = 1.5)
+#' plot(routeGeom2, lty = 2, add=TRUE)
+#' text(com[c(1,16),3:4], labels = com[c(1,16),2], pos = 2)
+#' 
+#' 
+#' 
+#' 
+#' 
 #' @export
-osrmViarouteGeom <- function(xo, yo, xd, yd){
+osrmViarouteGeom <- function(xo, yo, xd, yd, sp = FALSE, ido = "start", idd = "end"){
   tryCatch({
     # Query build
     tab <- paste(getOption("osrm.server"), "viaroute?loc=", sep = "")
@@ -43,10 +64,28 @@ osrmViarouteGeom <- function(xo, yo, xd, yd){
     
     geodf <- data.frame(lat = lats, lon = lons)
     
+    if (sp==TRUE){
+      if(!'package:sp' %in% search()){
+        attachNamespace('sp')
+      }
+      routeLines <- sp::Lines(slinelist = sp::Line(geodf[,2:1]), 
+                              ID = "x")
+      routeSL <- sp::SpatialLines(LinesList = list(routeLines), 
+                                  proj4string = sp::CRS("+init=epsg:4326"))
+      df <- data.frame(ido = ido, idd = idd)
+      geodf <- sp::SpatialLinesDataFrame(routeSL, 
+                                         data = df, 
+                                         match.ID = FALSE)   
+      row.names(geodf) <- paste(ido,idd,sep="_")
+
+    }
+    
     return(geodf)
   }, error=function(e) { message("osrmViarouteGeom function returns an error: \n", e)})
   return(NULL)
 }
+
+
 
 
 
