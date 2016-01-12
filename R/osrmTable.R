7931c5bddf79578ebeababf94bb5618e79fee6e3
rCarto <timothee.giraud@ums-riate.fr>
2016-01-12 17:26:56 +0100

    increase version number - breaking version

--- R/osrmTable.R
+++ R/osrmTable.R
@@ -2,71 +2,228 @@
 #' @title Get Travel Time Matrices Between Points
 #' @description Build and send OSRM API queries to get travel time matrices between points. 
 #' This function interface the \emph{table} OSRM service. 
-#' @param df data frame containing points identifiers, longitudes and latitudes 
-#' (WGS84).
-#' @param id identifier field in df.
-#' @param x longitude field in df.
-#' @param y latitude field in df.
-#' @return x A square matrix of time travel (in minutes) is returned.
-#' @note The public OSRM API does not allow more than 100 
-#' locations in distance table query. \cr
+#' @param loc data frame containing points identifiers, longitudes and latitudes 
+#' (WGS84). If loc parameter is used, all pair-wise distances are computed.
+#' @param locId identifier field in loc.
+#' @param locLat latitude field in loc.
+#' @param locLon longitude field in loc.
+#' @param src data frame containing origin points identifiers, longitudes and latitudes 
+#' (WGS84). If dst and src parameters are used, only pairs between scr/dst are computed.
+#' @param srcId identifier field in src
+#' @param srcLat latitude fiels in src
+#' @param srcLon longitude field in src
+#' @param dst data frame containing destination points identifiers, longitudes and latitudes 
+#' (WGS84). If dst and src parameters are used, only pairs between scr/dst are computed.
+#' @param dstId identifier field in dst
+#' @param dstLat latitude field in dst
+#' @param dstLon longitude field in dst
+#' @return A list containing 3 data frames is returned. 
+#' distance_table is the matrix of travel times (in minutes), 
+#' source_coordinates and destination coordinates are the coordinates of 
+#' the origin and destination points actually used to compute the travel times.
+#' @note The public OSRM API does not allow more than 10 000 
+#' distance in query result. \cr
 #' If you use an other OSRM API service, make sure that 
-#' more than 100 locations are allowed in table queries 
+#' more distances are allowed in results 
 #' (i.e. the "max-table-size" argument, Max. locations supported in distance 
 #' table query).
-#' @seealso \link{osrmTableOD}, \link{osrmTableErrors}
 #' @examples
-#' \dontrun{ 
+#' \dontrun{
 #' # Load data
 #' data("com")
 #' # Travel time matrix
-#' distcom <- osrmTable(com[1:50,], id = "comm_id", x =  "lon",y =  "lat")
+#' distCom <- osrmTable(loc = com[1:50,], 
+#'                      locId = "comm_id", 
+#'                      locLat = "lat", 
+#'                      locLon = "lon")
 #' # First 5 rows and columns
-#' distcom[1:5,1:5]
+#' distCom$distance_table[1:5,1:5]
+#' distCom2 <- osrmTable(src = com[1:50,], 
+#'                       srcId = "comm_id", srcLat = "lat", srcLon = "lon", 
+#'                       dst = com[51:100,], 
+#'                       dstId = "comm_id", dstLat = "lat", dstLon = "lon")
+#' # First 5 rows and columns
+#' distCom2$distance_table[1:5,1:5]
 #' }
 #' @export
-osrmTable <- function(df, id, x, y){
-  e <- simpleError("The public OSRM API does not allow more than 100 locations 
-                   in a distance table query.")
-  
-  if(getOption("osrm.server")=="http://router.project-osrm.org/" & nrow(df)>100){
-    stop(e)
-  }
-  
+osrmTable <- function(loc, locId, locLat, locLon, 
+                      src = NULL, srcId, srcLat, srcLon, 
+                      dst = NULL, dstId, dstLat, dstLon){
   tryCatch({
-    # Query build
-    tab <- paste(getOption("osrm.server"), "table?loc=", sep = "")
-    
-    n <- nrow(df)
-    for (i in 1:n){
-      tab <- paste(tab, df[i,y], ",", df[i,x], "&loc=", sep="")
+    if (is.null(src)){
+      nSrc <- nrow(loc)
+      nDst <- nSrc
+      osrmLimit(nSrc = nSrc, nDst = nDst)
+      # Build the query
+      req <- tableLoc(loc = loc, locLat = locLat, locLon = locLon)
+      # Get the result
+      resRaw <- RCurl::getURL(utils::URLencode(req), 
+                              useragent = "'osrm' R package")
+      # Parse the results
+      res <- jsonlite::fromJSON(resRaw)
+      # get the distance table
+      distance_table <- distTableFormat(res = res, 
+                                        src = loc, srcId = locId, 
+                                        dst = loc, dstId = locId)
+      # get the coordinates
+      coords <- coordFormat(res = res, 
+                            src = loc, srcId = locId, 
+                            dst = loc, dstId = locId)
+      return(list(distance_table = distance_table, 
+                  source_coordinates = coords$source_coordinates, 
+                  destination_coordinates = coords$destination_coordinates))
+      
+    } else {
+      nSrc <- nrow(src)
+      nDst <- nrow(dst)
+      osrmLimit(nSrc = nSrc, nDst = nDst)
+      # Build the query
+      req <- tableSrcDst(src = src, srcLat = srcLat, srcLon = srcLon, 
+                         dst = dst, dstLat = dstLat, dstLon = dstLon)
+      # Get the result
+      resRaw <- RCurl::getURL(utils::URLencode(req), 
+                              useragent = "'osrm' R package")
+      # Parse the results
+      res <- jsonlite::fromJSON(resRaw)
+      # get the distance table
+      distance_table <- distTableFormat(res = res, 
+                                        src = src, srcId = srcId, 
+                                        dst = dst, dstId = dstId)
+      # get the coordinates
+      coords <- coordFormat(res = res, 
+                            src = src, srcId = srcId, 
+                            dst = dst, dstId = dstId)
+      return(list(distance_table = distance_table, 
+                  source_coordinates = coords$source_coordinates, 
+                  destination_coordinates = coords$destination_coordinates))
     }
-    tab <- substr(x = tab, start = 1, stop = (nchar(tab)-5))
-
-    # Sending the query
-    tab2 <- RCurl::getURL(utils::URLencode(tab), useragent = "'osrm' R package")
-
-    # JSON parsing
-    # tab3 <- RJSONIO::fromJSON(content= tab2,default.size = 10)
-    # Conversion to matrix
-#     mat <- matrix(data = unlist( RJSONIO::fromJSON(content= tab2)[[1]]),
-#                   nrow = n,
-#                   ncol = n,
-#                   byrow = T,
-#                   dimnames =  list(df[,id], df[,id]))
-    mat <- jsonlite::fromJSON(tab2)$distance_table
-
-    dimnames(mat) <- list(df[,id], df[,id]) 
-    
-    # From millisec to minutes
-    mat <- round(mat/(600), 1)
-
-    # NA management
-    mat[mat == 3579139.4] <- NA
-
-    return(mat)
   }, error=function(e) { message("osrmTable function returns an error: \n", e)})
   return(NULL)
 }
 
+distTableFormat <- function(res, src, srcId, dst, dstId){
+  # extract distance table
+  mat <- res$distance_table
+  # From millisec to minutes
+  mat <- round(mat/(600), 1)
+  # NA management
+  mat[mat == 3579139.4] <- NA
+  # col and row names management
+  dimnames(mat) <- list(src[, srcId], dst[, dstId])
+  return(mat)
+}  
+
+coordFormat <- function(res, src, srcId, dst, dstId){
+  source_coordinates <- res$source_coordinates
+  row.names(source_coordinates) <- src[,srcId]
+  colnames(source_coordinates) <- c("Lat", "Lon")
+  destination_coordinates <- res$destination_coordinates
+  row.names(destination_coordinates) <- dst[,dstId]
+  colnames(destination_coordinates) <- c("Lat", "Lon")
+  return(list(source_coordinates = source_coordinates, 
+              destination_coordinates = destination_coordinates)
+  )
+}
+
+
+tableLoc <- function(loc, locLat, locLon){
+  # Query build
+  tab <- paste(getOption("osrm.server"), "table?loc=", sep = "")
+  n <- nrow(loc)
+  for (i in 1:n){
+    tab <- paste(tab, loc[i, locLat], ",", loc[i, locLon], "&loc=", sep="")
+  }
+  tab <- substr(x = tab, start = 1, stop = (nchar(tab)-5))
+  return(tab)
+}
+
+tableSrcDst <- function(src, srcLat, srcLon,
+                        dst, dstLat, dstLon){
+  # Query build
+  tab <- paste(getOption("osrm.server"), "table?src=", sep = "")
+  n <- nrow(src)
+  for (i in 1:n){
+    tab <- paste(tab, src[i,srcLat], ",", src[i,srcLon], "&src=", sep="")
+  }
+  tab <- substr(x = tab, start = 1, stop = (nchar(tab)-5))
+  tab <- paste(tab, "&dst=", sep = "")
+  m <- nrow(dst)
+  for (i in 1:m){
+    tab <- paste(tab, dst[i,dstLat], ",", dst[i,dstLon], "&dst=", sep="")
+  }
+  tab <- substr(x = tab, start = 1, stop = (nchar(tab)-5))
+  return(tab)
+}
+
+osrmLimit <- function(nSrc, nDst){
+  e <- simpleError("The public OSRM API does not allow results with 
+  a number of distances higher than 10000")
+  if(getOption("osrm.server") == "http://router.project-osrm.org/" & 
+     (nSrc*nDst) > 10000){
+    stop(e)
+  }
+}
+
+
+# src = com
+# srcId = "comm_id"
+# srcLat = "lat"
+# srcLon = "lon"
+# dst = com[1,]
+# dstId = "comm_id"
+# dstLat = "lat"
+# dstLon = "lon"
+# 
+# nSrc <- nrow(src)
+# nDst <- nrow(dst)
+# dimMat <- nSrc * nDst
+# d_t <- matrix(data = 1:dimMat, nrow = nSrc, ncol = nDst, 
+#               dimnames = list(src[,srcId], dst[, dstId]))
+# source_coordinates <- data.frame(row.names = src[,srcId], 
+#                                  Lat = rep(NA, nSrc), 
+#                                  Lon = rep(NA, nSrc))
+# destination_coordinates <- data.frame(row.names = dst[,dstId], 
+#                                       Lat = rep(NA, nDst), 
+#                                       Lon = rep(NA, nDst))
+# 
+# 
+# # nb de colonnes Full pour une requetes
+# nFullCol1Req <- 10000 %/% nSrc
+# 
+# # nb de requetes full necessaire
+# nReqFullCol <- nDst %/% nFullCol1Req
+# 
+# # nb col restantes
+# nColRest <- nDst - nReqFullCol * nFullCol1Req
+# 
+# init <- 1
+# for (i in 1:nReqFullCol){
+#   req <- tableSrcDst(src = src, srcLat = srcLat, srcLon = srcLon, 
+#                      dst = dst[init:(init+nFullCol1Req-1),], 
+#                      dstLat = dstLat, dstLon = dstLon)
+#   resRaw <- RCurl::getURL(utils::URLencode(req), 
+#                           useragent = "'osrm' R package")
+#   res <- jsonlite::fromJSON(resRaw)
+#   print(i)
+#   distance_table <- distTableFormat(res = res, 
+#                                     src = src, srcId = srcId, 
+#                                     dst = dst[init:(init+nFullCol1Req-1),], 
+#                                     dstId = dstId)
+#   d_t[,init:(init+nFullCol1Req-1)] <- distance_table
+#   init <- init+nFullCol1Req
+# }
+# 
+# # Pour les autres colonnes
+# req <- tableSrcDst(src = src, srcLat = srcLat, srcLon = srcLon, 
+#                    dst = dst[init:nDst,], 
+#                    dstLat = dstLat, dstLon = dstLon)
+# resRaw <- RCurl::getURL(utils::URLencode(req), 
+#                         useragent = "'osrm' R package")
+# res <- jsonlite::fromJSON(resRaw)
+# 
+# distance_table <- distTableFormat(res = res, 
+#                                   src = src, srcId = srcId, 
+#                                   dst = dst[init:nDst,], 
+#                                   dstId = dstId)
+# d_t[,init:nDst] <- distance_table
 
--- R/osrmViarouteGeom.R
+++ R/osrmViarouteGeom.R
@@ -2,13 +2,13 @@
 #' @title Get the Travel Geometry Between Two Points
 #' @description Build and send an OSRM API query to get the travel geometry between two points.
 #' This function interface the \emph{viaroute} OSRM service. 
-#' @param xo longitude of the origine point.
-#' @param yo latitude of the origine point.
-#' @param xd longitude of the destination point.
-#' @param yd latitude of the destination point.
+#' @param srcLon longitude of the origine point.
+#' @param srcLat latitude of the origine point.
+#' @param dstLon longitude of the destination point.
+#' @param dstLat latitude of the destination point.
 #' @param sp if sp is TRUE the function returns a SpatialLinesDataFrame.
-#' @param ido identifier of the origin point (use if sp is TRUE).
-#' @param idd identifier of the destination point (use if sp is TRUE).
+#' @param srcId identifier of the origin point (use if sp is TRUE).
+#' @param dstId identifier of the destination point (use if sp is TRUE).
 #' @return A data frame is return. It contains the longitudes and latitudes of 
 #' the travel path between the two points. If sp is TRUE a SpatialLinesDataFrame 
 #' is return. It contains two fields : identifiers of origine and destination.
@@ -18,19 +18,19 @@
 #' # Load data
 #' data("com")
 #' # Travel path between points
-#' routeGeom <- osrmViarouteGeom(xo = com[1,"lon"], yo = com[1,"lat"],
-#'                               xd = com[15,"lon"], yd = com[15,"lat"])
+#' routeGeom <- osrmViarouteGeom(srcLon = com[1,"lon"], srcLat = com[1,"lat"],
+#'                               dstLon = com[15,"lon"], dstLat = com[15,"lat"])
 #' # Display the path
 #' plot(com[c(1,15),3:4], asp =1, col = "red", pch = 20, cex = 1.5)
 #' points(routeGeom[,2:1], type = "l", lty = 2)
 #' text(com[c(1,15),3:4], labels = com[c(1,15),2], pos = 2)
 #' 
 #' # Travel path between points - output a SpatialLinesDataFrame
-#' routeGeom2 <- osrmViarouteGeom(xo = com[1,"lon"], yo = com[1,"lat"],
-#'                                xd = com[16,"lon"], yd = com[16,"lat"], 
+#' routeGeom2 <- osrmViarouteGeom(srcLon = com[1,"lon"], srcLat = com[1,"lat"],
+#'                                dstLon = com[16,"lon"], dstLat = com[16,"lat"], 
 #'                                sp=TRUE, 
-#'                                ido = com[1,"comm_id"], 
-#'                                idd = com[16,"comm_id"])
+#'                                srcId = com[1,"comm_id"], 
+#'                                dstId = com[16,"comm_id"])
 #' class(routeGeom2)
 #' # Display the path
 #' plot(com[c(1,16),3:4], asp =1, col = "red", pch = 20, cex = 1.5)
@@ -38,29 +38,35 @@
 #' text(com[c(1,16),3:4], labels = com[c(1,16),2], pos = 2)
 #' }
 #' @export
-osrmViarouteGeom <- function(xo, yo, xd, yd, sp = FALSE, ido = "start", idd = "end"){
+osrmViarouteGeom <- function(srcLat, srcLon, dstLat, dstLon, sp = FALSE, 
+                             srcId = "start", dstId = "end"){
   tryCatch({
-    # Query build
-    tab <- paste(getOption("osrm.server"), "viaroute?loc=", sep = "")
-    
-    tab <- paste(tab, yo, ",", xo, "&loc=",yd,",",xd, 
-                 "&alt=false&geometry=true&output=gpx",sep="")
+    # build the query
+    req <- paste(getOption("osrm.server"), 
+                 "viaroute?loc=", 
+                 srcLat, ",", srcLon, 
+                 "&loc=",
+                 dstLat,",",dstLon, 
+                 "&alt=false&geometry=true&",
+                 "output=json&compression=false",
+                 sep="")
     
     # Sending the query
-    tab2 <- RCurl::getURL(utils::URLencode(tab), useragent = "'osrm' R package")
+    resRaw <- RCurl::getURL(utils::URLencode(req), 
+                            useragent = "'osrm' R package")
     
-    # Parsing
-    pfile <- XML::htmlTreeParse(tab2, useInternalNodes = T)
+    # Parse the results
+    res <- jsonlite::fromJSON(resRaw)
     
-    # Get all elevations, times and coordinates via the respective xpath
-    coords <- XML::xpathSApply(pfile, path = "//rtept", XML::xmlAttrs)
+    # Error handling
+    e <- simpleError(res$status_message)
+    if(res$status != "200"){stop(e)}
     
-    # Extract latitude and longitude from the coordinates
-    lats <- as.numeric(coords["lat",])
-    lons <- as.numeric(coords["lon",])
+    # Coordinates of the line
+    geodf <- data.frame(res$route_geometry)
+    names(geodf) <-  c("lat", "lon")
     
-    geodf <- data.frame(lat = lats, lon = lons)
-    
+    # Convert to SpatialLinesDataFrame
     if (sp==TRUE){
       if(!'package:sp' %in% search()){
         attachNamespace('sp')
@@ -69,14 +75,12 @@ osrmViarouteGeom <- function(xo, yo, xd, yd, sp = FALSE, ido = "start", idd = "e
                               ID = "x")
       routeSL <- sp::SpatialLines(LinesList = list(routeLines), 
                                   proj4string = sp::CRS("+init=epsg:4326"))
-      df <- data.frame(ido = ido, idd = idd)
+      df <- data.frame(srcId = srcId, dstId = dstId)
       geodf <- sp::SpatialLinesDataFrame(routeSL, 
                                          data = df, 
                                          match.ID = FALSE)   
-      row.names(geodf) <- paste(ido,idd,sep="_")
-
+      row.names(geodf) <- paste(srcId, dstId,sep="_")
     }
-    
     return(geodf)
   }, error=function(e) { message("osrmViarouteGeom function returns an error: \n", e)})
   return(NULL)
