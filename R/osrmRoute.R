9cc856ed261a4284e08a0dce194637dc6984e909
rCarto <timothee.giraud@cnrs.fr>
2019-04-08 16:41:29 +0200

    sf support osrmroute

--- R/osrmRoute.R
+++ R/osrmRoute.R
@@ -3,18 +3,20 @@
 #' @description Build and send an OSRM API query to get the travel geometry between two points.
 #' This function interfaces the \emph{route} OSRM service. 
 #' @param src a numeric vector of identifier, longitude and latitude (WGS84), a 
-#' SpatialPointsDataFrame or a SpatialPolygonsDataFrame of the origine 
+#' SpatialPointsDataFrame, a SpatialPolygonsDataFrame or an sf object of the origine 
 #' point.
 #' @param dst a numeric vector of identifier, longitude and latitude (WGS84), a 
-#' SpatialPointsDataFrame or a SpatialPolygonsDataFrame of the destination 
+#' SpatialPointsDataFrame, a SpatialPolygonsDataFrame or an sf object of the destination 
 #' point.
 #' @param overview "full", "simplified" or FALSE. Add geometry either full (detailed), simplified 
 #' according to highest zoom level it could be display on, or not at all. 
 #' @param exclude pass an optional "exclude" request option to the OSRM API. 
-#' @param sp if sp is TRUE the function returns a SpatialLinesDataFrame.
-#' @return If sp is FALSE, a data frame is returned. It contains the longitudes and latitudes of 
+#' @param sp deprecated, if sp is TRUE the function returns a SpatialLinesDataFrame.
+#' @param returnclass if returnclass="sf" an sf LINESTRING is returned. 
+#' If returnclass="sp" a SpatialLineDataFrame is returned.
+#' @return If returnclass, a data frame is returned. It contains the longitudes and latitudes of 
 #' the travel path between the two points.\cr
-#' If sp is TRUE a SpatialLinesDataFrame is returned. It contains 4 fields : 
+#' If returnclass is TRUE a SpatialLinesDataFrame or an sf LINESTRING is returned. It contains 4 fields : 
 #' identifiers of origine and destination, travel time in minutes and travel distance in 
 #' kilometers.\cr
 #' If overview is FALSE, a named numeric vector is returned. It contains travel time (in minutes) 
@@ -63,12 +65,20 @@
 #' }
 #' @export
 osrmRoute <- function(src, dst, overview = "simplified", exclude = NULL,
-                      sp = FALSE){
+                      sp, returnclass){
   tryCatch({
+    if(!missing(sp)){
+      warning("sp is deprecated; use returnclass instead.", call. = FALSE)
+      if(sp){
+        returnclass <- "sp"
+      }
+    }
+    
     oprj <- NA
     if(testSp(src)){
       src <- sf::st_as_sf(src[1,])
-    }
+    }    
+    if(testSf(src)){
       oprj <- sf::st_crs(src)
       x <- sfToDf(x = src)
       src <- c(x[1,1],x[1,2], x[1,3])
@@ -76,20 +86,20 @@ osrmRoute <- function(src, dst, overview = "simplified", exclude = NULL,
     if(testSp(dst)){
       dst <- sf::st_as_sf(dst[1,])
     }
+    if(testSf(dst)){
+      oprj <- sf::st_crs(dst)
       x <- sfToDf(x = dst)
       dst <- c(x[1,1],x[1,2], x[1,3])
     }
     
-    exclude_str <- ""
-    if (!is.null(exclude)) { exclude_str <- paste("&exclude=", exclude, sep = "") }
-    
+    if (!is.null(exclude)) {exclude_str <- paste("&exclude=", exclude, sep = "") }
     # build the query
     req <- paste(getOption("osrm.server"),
                  "route/v1/", getOption("osrm.profile"), "/", 
                  src[2], ",", src[3], ";", dst[2],",",dst[3], 
                  "?alternatives=false&geometries=polyline&steps=false&overview=",
                  tolower(overview), exclude_str, sep="")
-
+    
     # Sending the query
     resRaw <- RCurl::getURL(utils::URLencode(req),
                             useragent = "'osrm' R package")
@@ -114,6 +124,7 @@ osrmRoute <- function(src, dst, overview = "simplified", exclude = NULL,
       return(round(c(duration = res$routes$duration/60,
                      distance = res$routes$distance/1000), 2))
     }
+    
     if(!vres){
       res$routes$geometry <- gsub(pattern = "zorglub", replacement = "\\\\",
                                   x = res$routes$geometry)
@@ -122,22 +133,23 @@ osrmRoute <- function(src, dst, overview = "simplified", exclude = NULL,
     geodf <- gepaf::decodePolyline(res$routes$geometry)[,c(2,1)]
     
     # Convert to SpatialLinesDataFrame
-    if (sp == TRUE){
-      routeLines <- sp::Lines(slinelist = sp::Line(geodf[,1:2]),
-                              ID = "x")
-      routeSL <- sp::SpatialLines(LinesList = list(routeLines),
-                                  proj4string = sp::CRS("+init=epsg:4326"))
-      df <- data.frame(src = src[1], dst = dst[1],
-                       duration = res$routes$legs[[1]]$duration/60,
-                       distance = res$routes$legs[[1]]$distance/1000)
-      geodf <- sp::SpatialLinesDataFrame(routeSL, data = df, match.ID = FALSE)
-      row.names(geodf) <- paste(src[1], dst[1],sep="_")
+    if (!missing(returnclass)){
+      rcoords <- paste0(geodf$lon, ' ', geodf$lat, collapse = ", ")
+      rgeom <- (st_as_sfc(paste0("LINESTRING(",rcoords,")")))
+      rosf <- st_sf(src = src[1], dst = dst[1],
+                    duration = res$routes$legs[[1]]$duration/60,
+                    distance = res$routes$legs[[1]]$distance/1000,
+                    geometry = rgeom, crs = 4326)
+      row.names(rosf) <- paste(src[1], dst[1],sep="_")
       if (!is.na(oprj)){
-        geodf <- sp::spTransform(geodf, oprj)
+        rosf <- sf::st_transform(rosf, oprj)
       }
-      names(geodf)[1:2] <- c("src", "dst")
+      names(rosf)[1:2] <- c("src", "dst")
+      if(returnclass=="sp"){
+        rosf <- methods::as(rosf, "Spatial")
+      }
     }
-    return(geodf)
+    return(rosf)
   }, error=function(e) {message("The OSRM server returned an error:\n", e)})
   return(NULL)
 }
