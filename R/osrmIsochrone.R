61e90da3e0207a413b0dfab216206865521c4a1d
pasipasi123 <pasi.haapakorva@gmail.com>
2020-02-22 10:22:03 +0200

    dmax

--- R/osrmIsochrone.R
+++ R/osrmIsochrone.R
@@ -184,7 +184,7 @@ osrmIsometric <- function(loc, breaks = seq(from = 0, to = 10000, length.out = 4
   if(options('osrm.profile')=="driving"){
     speed =  100 * 1000/60
   }
-  dmax <- tmax * speed * dmax_multiplier
+  dmax <- dmax_multiplier * max(breaks)
   
   # create a grid to obtain measures
   sgrid <- rgrid(loc = loc, dmax = dmax, res = res)
