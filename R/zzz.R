41fd8ee0c3d201b25b415f0d38035f3e19d36d95
rCarto <timothee.giraud@cnrs.fr>
2021-01-21 11:24:38 +0100

    Use .onLoad to set demo server instead of .onAttach

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,10 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("Data: (c) OpenStreetMap contributors, ODbL 1.0 - http://www.openstreetmap.org/copyright")
   packageStartupMessage("Routing: OSRM - http://project-osrm.org/")
-  options(osrm.server = "https://routing.openstreetmap.de/", osrm.profile = "car")
   # options(osrm.server = "http://0.0.0.0:5000/", osrm.profile = "driving")
 }
 
+.onLoad <- function(libname, pkgname) {
+  options(osrm.server = "https://routing.openstreetmap.de/", osrm.profile = "car")
+  # options(osrm.server = "http://0.0.0.0:5000/", osrm.profile = "driving")
+}
