f05b01c2b66f2510f07856eae8573ef73f5c21d6
rCarto <timothee.giraud@cnrs.fr>
2021-09-27 13:55:54 +0200

    replace RData sample dataset by raw files

--- R/package.R
+++ R/package.R
@@ -27,33 +27,15 @@
 #' \code{options(osrm.server = "http://0.0.0.0:5000/", osrm.profile = "car")}
 #' 
 #' 
+#' 
+#' 
+#' The package ships a sample dataset of 100 random pharmacies in Berlin 
+#' (© OpenStreetMap contributors - \url{https://www.openstreetmap.org/copyright/en}).   
+#' The sf dataset uses the projection WGS 84 / UTM zone 34N (EPSG:32634).
+#' The csv dataset uses WGS 84 (EPSG:4326). 
+#' 
+#' 
+#' 
 #' @docType package
 NULL
 
-
-#' @title sf POINT of 100 Random Pharmacies in Berlin
-#' @name apotheke.sf
-#' @description 100 random pharmacies in Berlin. 
-#' The projection is WGS 84 / UTM zone 34N.
-#' @import sf
-#' @source © OpenStreetMap contributors - \url{https://www.openstreetmap.org/copyright/en}.
-#' @docType data
-NULL
-
-
-#' @title SpatialPointsDataFrame of 100 Random Pharmacies in Berlin
-#' @name apotheke.sp
-#' @description 100 random pharmacies in Berlin. 
-#' The projection is WGS 84 / UTM zone 34N.
-#' @import sp
-#' @source © OpenStreetMap contributors - \url{https://www.openstreetmap.org/copyright/en}.
-#' @docType data
-NULL
-
-#' @title Coordinates of 100 Random Pharmacies in Berlin
-#' @name apotheke.df
-#' @description A data.frame of coordinates of 100 random pharmacies in Berlin. 
-#' The projection is WGS 84.
-#' @source © OpenStreetMap contributors - \url{https://www.openstreetmap.org/copyright/en}.
-#' @docType data
-NULL
